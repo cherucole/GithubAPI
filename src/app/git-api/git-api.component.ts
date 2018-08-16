38258751302e44b868e4635d5ad1b7787e7719ba
cherucole <cheruyotcheruyot@gmail.com>
2018-08-16 16:42:05 +0300

    added components and 2 class files

---
+++ src/app/git-api/git-api.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-git-api',
+  templateUrl: './git-api.component.html',
+  styleUrls: ['./git-api.component.css']
+})
+export class GitApiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
