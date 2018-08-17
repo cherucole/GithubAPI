ae3c847bd2513fe4fab5217553dfcbb8b8c45f2e
cherucole <cheruyotcheruyot@gmail.com>
2018-08-17 18:31:03 +0300

    working on my directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 
 import { AppComponent } from './app.component';
 import { GitApiComponent } from './git-api/git-api.component';
-import {GitApiService} from './git-api.service';
+import { GitApiService } from './git-api.service';
 
 @NgModule({
   declarations: [
